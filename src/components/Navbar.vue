656494508d75948686762e6fc43dda5ce0e9f08b
Şafak Dinç <safakdinc02@gmail.com>
2023-04-23 01:27:02 +0300

    background and PostsView update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,38 +1,45 @@
 <template>
-  <nav class="flex justify-between bg-[rgb(23,23,23)] text-gray-50">
-    <!-- logo, for later -->
-    <div class="logo flex cursor-pointer ml-2 justify-center items-center">
-      <router-link to="/">
-        <p class="text-xl md:text-2xl font-bold select-none">𐰅𐰑𐰅𐰉𐰄𐰖𐰀𐱃𐰄.𐰗𐰺𐰏</p>
-      </router-link>
-    </div>
-    <!-- menus -->
-    <div v-if="!isAuth" class="gap-2 mr-2 flex items-center">
-      <div v-for="link in navbar">
-        <router-link :to="link.path">
-          <font-awesome-icon :icon="['fas', link.icon]"></font-awesome-icon>
-          <span class="underline px-1 py-2 cursor-pointer">{{
-            link.name
-          }}</span>
+  <div class="fixed w-full top-0 left-0" ref="navbar2" style="z-index: 10">
+    <nav class="flex justify-between bg-[rgb(23,23,23)] text-gray-50">
+      <!-- logo, for later -->
+      <div class="logo flex cursor-pointer ml-2 justify-center items-center">
+        <router-link to="/">
+          <p class="text-xl md:text-2xl font-bold select-none">𐰅𐰑𐰅𐰉𐰄𐰖𐰀𐱃𐰄.𐰗𐰺𐰏</p>
         </router-link>
       </div>
-    </div>
-    <!-- mobile menu -->
-    <div v-if="isAuth" class="flex">
-      <NavbarMobile></NavbarMobile>
-    </div>
-  </nav>
+      <!-- menus -->
+      <div v-if="!isAuth" class="gap-2 mr-2 flex items-center">
+        <div v-for="link in navbar">
+          <router-link :to="link.path">
+            <font-awesome-icon :icon="['fas', link.icon]"></font-awesome-icon>
+            <span class="underline px-1 py-2 cursor-pointer">{{ link.name }}</span>
+          </router-link>
+        </div>
+      </div>
+      <!-- mobile menu -->
+      <div v-if="isAuth" class="flex">
+        <NavbarMobile></NavbarMobile>
+      </div>
+    </nav>
+  </div>
 </template>
 <script setup>
-import navbar from "./Navbar.js";
-import { modalstate } from "../modalState";
-import NavbarMobile from "./NavbarMobile.vue";
-import { ref, onMounted } from "vue";
-import { verifyToken } from "../utils/jwt.js";
+import navbar from './Navbar.js'
+import { modalstate } from '../modalState'
+import NavbarMobile from './NavbarMobile.vue'
+import { ref, onMounted } from 'vue'
+import { verifyToken } from '../utils/jwt.js'
 
-const isAuth = ref(false);
+const emits = defineEmits(['marginTopFunction'])
+const navbar2 = ref(null)
+const isAuth = ref(false)
 onMounted(() => {
-  isAuth.value = Boolean(localStorage.getItem("currentUser"));
-  console.log(isAuth.value);
-});
+  isAuth.value = Boolean(localStorage.getItem('currentUser'))
+})
+onMounted(async () => {
+  setTimeout(() => {
+    const height = navbar2.value.offsetHeight
+    emits('marginTopFunction', height)
+  }, 200)
+})
 </script>
--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,39 +1,31 @@
 <template>
   <div class="w-full grid grid-cols-12">
     <div class="col-span-12 lg:col-span-8 lg:col-start-3">
-      <template v-for="post in posts">
+      <template v-for="(post, index) in posts">
         <section>
           <div class="max-h-(screen-h-50) background rounded-lg mt-5">
             <!-- Info Top section -->
-            <div class="-200 p-4">
-              <span class="text-black"
-                >Posted on April 1st, 2023 by John Doe</span
-              >
+            <div class="-200 p-4 flex justify-between">
+              <span class="text-black">John Doe tarafından {{ post.fulldate }} tarihinde gönderildi</span>
+              <div>
+                <span>{{ post.views }} defa görüntülendi</span>
+              </div>
             </div>
             <!-- Author section -->
             <div class="flex p-4 w-full">
               <div class="flex items-center w-6/12">
-                <img
-                  class="w-12 h-12 rounded-full mr-4"
-                  src="../assets/profile.png"
-                  alt="Author Photo"
-                />
+                <img class="w-12 h-12 rounded-full mr-4" src="../assets/profile.png" alt="Author Photo" />
                 <div>
                   <h2 class="text-lg font-semibold text-gray-700">John Doe</h2>
-                  <p class="text-gray-700 justify-self-end">
-                    Author of this article
-                  </p>
+                  <p class="text-gray-700 justify-self-end">Bu Makalenin Yazarı</p>
                 </div>
               </div>
               <div class="h-full text-slate-100 w-6/12 text-xs">
                 <div class="flex flex-wrap justify-end">
-                  <div
-                    class="mr-2 mt-1"
-                    style="height: fit-content; width: fit-content"
-                  >
-                    <a href="#" target="_blank">
+                  <div v-for="(tag, index) in post.tags" class="mr-2 mt-1" style="height: fit-content; width: fit-content">
+                    <a :href="'#' + tag" target="_blank">
                       <div class="tag-box p-1 h-fit flex items-center">
-                        Tags
+                        {{ tag }}
                       </div>
                     </a>
                   </div>
@@ -42,38 +34,27 @@
             </div>
             <!-- Image section -->
             <div class="" style="height: 70vh">
-              <img
-                class="h-full w-full object-cover object-center rounded-lg"
-                src="../assets/image.jpg"
-                alt="Main Image"
-              />
+              <img class="h-full w-full object-cover object-center rounded-lg" :src="post.image" alt="Main Image" />
             </div>
             <!-- Title section -->
             <div class="pl-4 pr-4">
-              <h3 class="text-2xl font-semibold text-black">Title</h3>
+              <h3 class="text-2xl font-semibold text-black">{{ post.title }}</h3>
             </div>
 
             <!-- Preview section -->
-            <div
-              class="px-4 text-gray-900 overflow-hidden transition-all duration-200 ease-in-out"
-              ref="container"
-            >
-              <!--<div ref="longabbr" :hidden="data.showmore != true" v-html="'<p>' + data.longabbr + '</p>'"></div>-->
-              <!--<div ref="abbr" :hidden="data.showmore == true" v-html="data.abbr + '...'"></div>-->
-              <div ref="abbr">Content</div>
+            <div class="px-4 text-gray-900 overflow-hidden transition-all duration-200 ease-in-out" ref="container">
+              <!--<div ref="longabbr" :hidden="post.showmore != true" v-html="'<p>' + post.longabbr + '</p>'"></div>-->
+              <!--<div ref="abbr" :hidden="post.showmore == true" v-html="post.abbr + '...'"></div>-->
+              <div ref="abbr" v-html="post.content.html"></div>
             </div>
-            <!--<div v-if="data.showmore == true" class="font-bold pl-4 show-more" @click="showless(index)">Show Less</div>-->
-            <div class="font-bold pl-4 show-more" @click="showmore(index)">
-              Show More
-            </div>
+            <!--<div v-if="data.showmore == true" class="font-bold pl-4 show-more" @click="showless(index)">Daha Az</div>-->
+            <div class="font-bold pl-4 show-more" @click="showmore(index)">Daha Fazla</div>
             <div id="container" class="mt-2">
               <button class="learn-more">
                 <span class="circle flex items-center" aria-hidden="true">
                   <span class="icon arrow"></span>
                 </span>
-                <span class="button-text flex items-center ml-8"
-                  >Learn More</span
-                >
+                <span class="button-text flex items-center ml-8">Okumaya Devam Et</span>
               </button>
             </div>
 
@@ -82,26 +63,20 @@
               <div class="flex justify-between items-center">
                 <div>
                   <button class="mr-2 button">
-                    <i
-                      class="fa-solid fa-thumbs-up fa-lg ease duration-300 text-gray-400 hover:text-white"
-                    ></i>
+                    <font-awesome-icon :icon="['fas', 'thumbs-up']" size="lg" class="ease duration-300 text-black hover:text-gray-600" />
+                    {{ post.likes }}
                   </button>
                   <button class="mr-2 button">
-                    <i
-                      class="fa-solid fa-thumbs-down fa-lg ease duration-300 text-gray-400 hover:text-white"
-                    ></i>
+                    <font-awesome-icon :icon="['fas', 'thumbs-down']" size="lg" class="ease duration-300 text-black hover:text-gray-600" />
+                    {{ post.dislikes }}
                   </button>
                   <button class="button">
-                    <i
-                      class="fa-solid fa-comments fa-lg ease duration-300 text-gray-400 hover:text-white"
-                    ></i>
+                    <font-awesome-icon :icon="['fas', 'comments']" size="lg" class="ease duration-300 text-black hover:text-gray-600" />
                   </button>
                 </div>
                 <div>
                   <button>
-                    <i
-                      class="fa-sharp fa-light fa-share fa-lg ease duration-300 text-gray-400 hover:text-white"
-                    ></i>
+                    <font-awesome-icon :icon="['fas', 'share']" size="lg" class="ease duration-300 text-black hover:text-gray-600" />
                   </button>
                 </div>
               </div>
@@ -113,22 +88,41 @@
   </div>
 </template>
 <script setup>
-import "../../src/assets/learnmore.scss";
-import axiosUtil from "../utils/axios.js";
-import { onMounted, ref } from "vue";
-let page = ref(1);
-let posts = ref([]);
+import '../../src/assets/learnmore.scss'
+import axiosUtil from '../utils/axios.js'
+import { onMounted, ref } from 'vue'
+let page = ref(1)
+let posts = ref([])
 async function fetchPost(page) {
-  axiosUtil.get("/post/fetchall/" + page).then((result) => {
-    console.log(result);
-    posts.value.push(result.data);
-    let a = result.data;
-    console.log(a);
-  });
+  axiosUtil.get('/post/fetchall/' + page).then(result => {
+    result.data.forEach(element => {
+      posts.value.push(element)
+    })
+    posts.value.forEach(post => {
+      ///write sayfasından gönderilen post olduğu zaman commentları kaldır
+      /*post.longabbr = data.content.split('<p>')[1].split('</p>')[0]
+      let array = data.content.split(' ').slice(0, 26)
+      let string = array.join(' ')
+      post.abbr = string*/
+      let date = new Date(post.created_at * 1000)
+      let month = date.toLocaleString('tr-TR', { month: 'long' })
+      let day = date.toLocaleString('tr-TR', { day: 'numeric' })
+      let year = date.toLocaleString('tr-TR', { year: 'numeric' })
+      let fulldate = day + ' ' + month + ' ' + year
+      post.fulldate = fulldate
+    })
+  })
+}
+const showmore = index => {
+  //posts[index].showmore = true
+  console.log(index)
+}
+const showless = index => {
+  posts[index].showmore = false
 }
 onMounted(async () => {
-  await fetchPost(page.value);
-});
+  await fetchPost(page.value)
+})
 </script>
 <style scoped>
 .button {
